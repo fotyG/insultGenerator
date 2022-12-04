7c8bf1a17c4940466fb38972ad127258b574fa10
Shota Giorgadze <giorgadze@inbox.lv>
2022-12-04 14:14:42 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const insults = {
+    hair: ["Looks like you glued a raccoon to your head..", "Did you lick a wall socket this morning by any chance?", "If I had to give your haircut a name - it would be: 'an explosion on a pasta factory'!", "It seems like your hair went for a walk and didn't leave a goodbye note.."],
+    face: ["Did you have a fight with a UFC fighter? You obviously lost..", "For how long was your head stuck in a beehive?", "My 110 years old grandpa hasn't been sober since he was 5 - your faces look very similar", "I think your face was designed on a 1st of April"],
+    outfit: ["Was Ronald McDonald angry that you took his outfit?", "I never knew a potato bag could be worn like this", "You broke the laws of physics by fitting S-Size kid's outfit on your 300kg body", "Nice outfit for Halloween - I can clearly tell you dressed up as a floor mop! What.. It's not Halloween yet? Oh.."]
+};
+console.log(insults.hair[3], insults.face[3]);
